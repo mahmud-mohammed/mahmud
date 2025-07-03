b2eb23e373edd8cc2ac80a579090f3d4e4c9339f
mahmud-mohammed <mahmudmohammed2004@gmail.com>
2025-07-03 11:52:24 +0100

    datatype

--- Overview/DataTypes/Program.cs
+++ Overview/DataTypes/Program.cs
@@ -49,10 +49,10 @@ void ClassOp(MyClass theClass) {
 Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
 Console.ReadLine();
 
-// Console.WriteLine("Objects are passed by reference, since they are reference types:");
-// Console.WriteLine($"{cl.a}, {cl.b}");
-// ClassOp(cl);
-// Console.WriteLine($"{cl.a}, {cl.b}");
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}");
+ClassOp(cl);
+ Console.WriteLine($"{cl.a}, {cl.b}");
 
 // These are declared at the bottom of the file because C# requires
 // top-level statements to come before type declarations
